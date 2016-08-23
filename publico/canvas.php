643c0da1c62e07c5dc6d77c271da1e0ee9c4f9e7
GabiToRawr <gabrielalejandro84@hotmail.com>
2016-08-23 03:48:19 -0600

    Agrego lo del canvas

---
+++ publico/canvas.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Prueba del Canvas</title>
+	<?php include 'links.php'; ?>
+	<link rel="stylesheet" type="text/css" href="publico/css/canvas.css">
+	<script src="publico/js/fabric.min.js"></script>	
+	<script src="publico/js/jscolor.min.js"></script>	
+	<script src="publico/js/FileSaver.min.js"></script>
+	<script src="publico/js/canvas-toBlob.js"></script>
+</head>
+<body>
+	<?php include 'menu.php' ?>
+	<div class="uk-grid">
+		<div class="uk-width-1-1 titulo-f uk-text-center uk-text-large uk-text-middle header">
+			<h3 class="titulo-f">EDICION DE PRODUCTOS</h3>			
+		</div>		
+	</div>
+	<div class="uk-grid"> <!--Grid para el canvas y los botones superiores a este-->
+		<div class="uk-width-7-10">
+			<div class="uk-grid"> <!--Primera fila de botones superiores-->
+				<div class="uk-width-2-3  ">
+					<form action="" class="uk-form">
+						<fieldset data-uk-margin>
+							<label for="editando">Editando:</label> <!--Seccion determinante del fondo del canvas-->
+							<input id="editando" type="text" placeholder="" disabled class="uk-form-width-large uk-form-help-inline uk-form-horizontal" value="Camisa"> <!--El value determinará lo que se editara-->
+							<input type="hidden" class="" name="emailUser" value="<?php echo $_SESSION['email'];?>">
+						</fieldset>
+					</form>
+				</div> <!--Para informacion de lo que se esta editando-->
+				<br><br>
+				<div class="uk-width-1-3  ">
+					<div class="btn-group">
+					  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					    Cambiar <span class="caret"></span>
+					  </button>
+					  <ul class="dropdown-menu">
+					    <li><a onclick="CambiarEditando(1)">Camisa</a></li>
+					    <li><a onclick="CambiarEditando(2)">Taza</a></li>
+					  </ul>
+					</div>
+				</div> <!--Para el boton de cambiar lo que estoy editando-->
+				<br>
+				<div class="uk-width-1-4 "><a class="uk-icon-hover uk-icon-save uk-icon-large" id="Saves"> Guardar</a></div>
+				<div class="uk-width-1-4 ">
+					<div class="uk-button-dropdown" data-uk-dropdown="{pos: 'right-top'}">
+						<a class="uk-icon-hover uk-icon-pencil uk-icon-large" onclick="habilitarDibujo('69')"> Dibujo</a>
+						<div class="uk-dropdown">
+							<a onclick="habilitarDibujo('Pencil')" id="br1" class="uk-icon-large">Lapiz</a><br><br>
+							<a onclick="habilitarDibujo('Circle')" id="br2" class="uk-icon-large">Circulos</a><br><br>
+							<a onclick="habilitarDibujo('Spray')" id="br3" class="uk-icon-large">Spray</a><br><br>
+							<a onclick="habilitarDibujo('Pattern')" id="br4" class="uk-icon-large">Patron de Circulos</a>
+						</div>
+					</div>
+				</div>
+				<div class="uk-width-1-4 ">
+					<div class="uk-button-dropdown" data-uk-dropdown="{pos:'right-top'}">
+						<a class="uk-icon-hover uk-icon-close uk-icon-large"> Eliminar</a>
+						<div class="uk-dropdown">
+							<a class="uk-icon-large" onclick="eliminar()">Eliminar Elemento</a><br><br>
+							<a class="uk-icon-large" onclick="clearCanvas()">Limpiar</a><br><br>
+						</div>
+					</div>
+				</div>
+				<div class="uk-width-1-4 ">
+					<div class="uk-button-dropdown" data-uk-dropdown="{pos:'right-top'}">
+						<a class="uk-icon-hover uk-icon-cube uk-icon-large "> Formas</a>
+						<div class="uk-dropdown">
+							<a onclick="crearCuadro()" class="uk-icon-hover uk-icon-square uk-icon-large">  Cuadrado</a><br><br>
+							<a onclick="crearTriangulo()" class="uk-icon-hover uk-icon-warning uk-icon-large"> Triangulo</a><br><br>
+							<a onclick="crearCirculo()" class="uk-icon-hover uk-icon-circle uk-icon-large">  Circulo</a><br><br>
+							<a onclick="crearLinea()" class="uk-icon-hover uk-icon-minus uk-icon-large">   Linea</a>
+						</div>
+					</div>
+				</div>
+				<br><br>
+				<div class="uk-width-1-1 canvas">
+					<canvas id="maincanvas" class="maincanvas" width="940" height="510"></canvas><!--Canvas de edicion-->
+				</div>
+			</div>
+		</div>
+		<div class="uk-width-3-10"> <!--Herramientas laterales de edicion e insercion--> <br>
+			<br>
+			<div class="uk-grid">
+				<div class="uk-width-1-3"><button value="#000000" id="colorpicker" class="jscolor {valueElement:null, onFineChange:'update(this)', width:243, height:150, position:'right',
+    		borderColor:'#FFF', insetColor:'#FFF', backgroundColor:'#666'}">Elige un Color</button></div>
+				<div class="uk-width-2-3 uk-form">
+					<p>Tipo de Letra: </p>
+					<select id="tipo_letra" name="cmbTipoLetra" onchange="CambiarLetra();">
+						<option> Serif </option>
+						<option> Arial </option>
+						<option> Sans-Serif </option>                                  
+						<option> Tahoma </option>
+						<option> Verdana </option>
+						<option> Lucida Sans Unicode </option>                               
+					</select>
+				</div>
+				<br><br><br><br>
+				<div class="uk-width-1-1 uk-form">
+					<div class="uk-form-row">
+						<p>Texto a agregar:</p>
+					    <textarea class="txtA-des" id="txtdes" cols="40" rows="3" placeholder=""></textarea>
+					</div>
+					<br>
+					<div class="centrado">
+						<label for="fsize">Tamaño</label>
+						<input type="number" onchange="actualizarBrush()" id="fsize" value="8" name="quantity" min="1" max="100">
+						<button class="uk-button-primary addte" type="button" onclick="añadirTexto()">Añadir el texto</button>
+					</div>					
+				</div>
+			</div>
+			<div class="uk-grid">
+				<div class="uk-width-1-1">
+					<div class="bloque">
+						<br>
+						<input type="file" onchange="readURL(this,1);" name="subir1" id="subir1" accept="image/*"><br>
+						<div class="centerbtn"><button class="uk-button-primary " type="button" id="btns1" onclick="añadirImagen(1)">Añadir la imagen</button></div><br>
+						<input type="file" onchange="readURL(this,2);" name="subir2" id="subir2" accept="image/*"><br>
+						<div class="centerbtn"><button class="uk-button-primary " type="button" id="btns2" onclick="añadirImagen(2)">Añadir la imagen</button></div><br>
+						<input type="file" onchange="readURL(this,3);" name="subir3" id="subir3" accept="image/*"><br>
+						<div class="centerbtn"><button class="uk-button-primary " type="button" id="btns3" onclick="añadirImagen(3)">Añadir la imagen</button></div><br>
+    					<img id="hid1" class="hidden" src="" alt="your image" />
+    					<img id="hid2" class="hidden" src="" alt="your image" />
+    					<img id="hid3" class="hidden" src="" alt="your image" />
+					</div>
+				</div>
+			</div>			
+		</div>
+	</div>		
+	<?php include 'footer.php' ?>
+	<?php include 'scripts.php' ?>
+	<script type="text/javascript" src="publico/js/canvas.js"></script>
+</body>
+</html>
---
+++ publico/js/canvas.js
@@ -0,0 +1,204 @@
+function readURL(input,number) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            var numberparametro = number;
+            reader.onload = function (e) {
+                $('#hid' + numberparametro)
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+//////////////////////////////////////////////////
+var canvas = new fabric.Canvas("maincanvas",{hoverCursor: 'pointer'});
+canvas.selectionColor = 'rgba(0,255,0,0.3)';
+  canvas.selectionBorderColor = 'red';
+  canvas.selectionLineWidth = 2;
+canvas.on({
+    'object:moving': function(e) {
+      e.target.opacity = 0.5;
+      //onChange(e);
+    },
+    'object:modified': function(e) {
+      e.target.opacity = 1;
+    }
+    //'object:scaling': onChange,
+    //'object:rotating': onChange,
+  });
+var color_HEX;
+var color_RGB;
+var edit_num;
+var dibujando = false;
+var brush;
+function render(){
+	canvas.renderAll();
+}
+function clearCanvas(){
+	//habilitar botones, limpiar canvas
+	canvas.clear();
+	cargarFondo();
+	for (var i = 1; i <= 3; i++) {
+	document.getElementById("btns" + i).removeAttribute('disabled');
+	document.getElementById("btns" + i).title = "";		
+	};	
+}
+function eliminar(){
+	if (canvas.getActiveGroup() != null){
+		alert("elimine los elementos uno por uno");
+		canvas.discardActiveGroup();
+	}
+	else{
+		canvas.remove(canvas.getActiveObject());
+	}	
+	
+}
+function update(picker) {
+    color_HEX = picker.toHEXString();
+    color_RGB = picker.toRGBString(); 
+    actualizarBrush();   
+    }
+function cargarFondo(identificador){
+	var Editando = document.getElementById("editando").value;	
+	switch (Editando){
+		case "Camisa":  //Editando una camisa
+		canvas.setBackgroundImage("publico/img/camisablanca3.png");
+		break;
+		case "Taza": //Editando una taza
+		canvas.setBackgroundImage("publico/img/taza3-bak.png");
+		break;
+		default:		
+		alert("Escoga algo que editar");
+	}
+}
+function actualizarBrush(){
+	canvas.freeDrawingBrush.color = color_HEX;
+    canvas.freeDrawingBrush.width = parseInt(document.getElementById("fsize").value);
+}
+
+function habilitarDibujo(id){
+	if (id != 69){
+		if (!dibujando) {
+			canvas.isDrawingMode = true;
+			dibujando = true;
+			brush = id;
+			canvas.freeDrawingBrush = new fabric[brush + 'Brush'](canvas);
+			if (canvas.freeDrawingBrush) {
+			      actualizarBrush();
+			    }
+		}	
+	}
+	else{
+		canvas.isDrawingMode = false;
+		dibujando = false;
+	}
+}
+function CambiarEditando(identificador){
+	switch (identificador){
+		case 1:
+			document.getElementById("editando").setAttribute('value','Camisa');
+			clearCanvas();
+
+			break;
+		case 2:
+			document.getElementById("editando").setAttribute('value','Taza');
+			clearCanvas();
+			break;
+		default:
+			alert("no disponible");
+			break;
+	}
+	cargarFondo();
+	//limpiar todo el canvas
+}
+
+function CambiarLetra() {		 // Este solo cambia la letra del text Area
+        var cmb = document.getElementById('tipo_letra');
+        var family = cmb.options[cmb.selectedIndex].value;
+        var txtA = document.getElementById('txtdes')
+        txtA.style.fontFamily = family;       
+      }
+function añadirTexto(){
+	var texto = document.getElementById("txtdes").value;
+	var cmb = document.getElementById('tipo_letra');
+	var family = cmb.options[cmb.selectedIndex].value;
+	if (texto != ""){
+		var textTOadd = new fabric.Text("" + texto, { left: 100, top: 100, fontSize: parseInt(document.getElementById("fsize").value),
+			fontFamily: family, fill: "" + color_HEX, borderColor: 'black', cornerColor: 'purple',
+			cornerSize: 15, transparentCorners: true });
+		canvas.add(textTOadd);
+	}		
+	render();
+}
+function añadirImagen(idnum_img){
+	var imagen = document.getElementById("hid" + idnum_img);
+	var srcimg = document.getElementById("hid" + idnum_img).src;
+	if (imagen != null && (srcimg != "#" && srcimg != "" && !srcimg.includes("http"))){ //verificar eso de includes con el server
+		//si la imagen ya fue cargada
+		var imagenInstancia = new fabric.Image(imagen,{
+		left: 100,
+  		top: 100,
+  		width: 200,
+  		height: 200,
+  		borderColor: 'black', cornerColor: 'purple',
+		cornerSize: 15, transparentCorners: true
+		});
+		canvas.add(imagenInstancia);
+		render();
+		document.getElementById("btns" + idnum_img).disabled = "true";
+		document.getElementById("btns" + idnum_img).title = "Se Habilita al Limpiar";
+	}
+	else{
+		alert("Escoga una imagen que agregar");
+	}		
+}
+function crearCirculo(){
+	var circle = new fabric.Circle({
+  		radius: 40, fill: "" + color_HEX, left: 100, top: 100, borderColor: 'black', cornerColor: 'purple',
+		cornerSize: 15, transparentCorners: true
+	});
+	canvas.add(circle);
+	render();
+}
+function crearTriangulo(){
+	var triangle = new fabric.Triangle({
+	  width: 80, height: 80, fill: "" + color_HEX, left: 100, top: 100, borderColor: 'black', cornerColor: 'purple',
+		cornerSize: 15, transparentCorners: true
+	});
+	canvas.add(triangle);
+	render();
+}
+function crearCuadro(){
+	var cuadro = new fabric.Rect({
+		width: 80, height:80, fill: "" + color_HEX, left: 100, top: 100, borderColor: 'black', cornerColor: 'purple',
+		cornerSize: 15, transparentCorners: true
+	});
+	canvas.add(cuadro);
+	render();
+}
+function crearLinea(){
+	var linea = new fabric.Line({
+		height: 40, width: 12, fill: "" + color_HEX, left: 100, top: 100, borderColor: 'black', cornerColor: 'purple',
+		cornerSize: 15, transparentCorners: true
+	});
+	canvas.add(linea);
+	render();
+}
+
+$("#Saves").click(function(){
+	$("#maincanvas").get(0).toBlob(function(blob){
+		saveAs(blob,"Diseño_Personalizado_" + document.getElementById("editando").value +".png");
+	});
+});
+
+function onChange(options) { //interponer las figuras/imagenes
+    options.target.setCoords();
+    canvas.forEachObject(function(obj) {
+      if (obj === options.target) return;
+      obj.setOpacity(options.target.intersectsWithObject(obj) ? 0.5 : 1);
+    });
+  }
+cargarFondo();
+render();
