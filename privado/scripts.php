13ca51710cd758d764072d3282faf0e0250e0b81
OSLEZ <oslizama@gmail.com>
2016-09-05 19:37:24 -0600

    SCRIPTS

--- privado/scripts.php
+++ privado/scripts.php
@@ -1,29 +1,29 @@
 <!--SCRIPT PARA EL JQUERY-->
-<script type="text/javascript" src="vendor/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/jquery-1.11.2.min.js"></script>
 <!--ENLAZO EL SCRIPT DEL MENÚ-->
-<script type="text/javascript" src="vendor/uikit.min.js"></script>
-<script type="text/javascript" src="components/upload.js"></script>
-<script type="text/javascript" src="vendor/menu_backend.js"></script>
-<script type="text/javascript" src="vendor/acciones.js"></script>
-<script type="text/javascript" src="vendor/checks.js"></script>
-<script type="text/javascript" src="vendor/seleccionar.js"></script>
-<script type="text/javascript" src="vendor/mostrar_tbl.js"></script>
-<script type="text/javascript" src="vendor/sweetalert.min.js"></script>
-<script type="text/javascript" src="vendor/bootstrap.js"></script>
-<script type="text/javascript" src="vendor/tooltip.js"></script>
-<script type="text/javascript" src="vendor/vista-previa.js"></script>
-<script type="text/javascript" src="vendor/empresap.js"></script>
-<script type="text/javascript" src="vendor/busqueda.js"></script>
-<script type="text/javascript" src="vendor/perfilbk.js"></script>
-<script type="text/javascript" src="vendor/comentariobk.js"></script>
-<script type="text/javascript" src="vendor/sesionpv.js"></script>
-<script type="text/javascript" src="vendor/uploadjs.js"></script>
-<script type="text/javascript" src="vendor/bootstrap-datepicker.js"></script>
-<script type="text/javascript" src="vendor/selecimg.js"></script>
-<script type="text/javascript" src="vendor/msjclient.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/uikit.min.js"></script>
+<script type="text/javascript" src="/publico/js/components/upload.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/menu_backend.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/acciones.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/checks.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/seleccionar.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/mostrar_tbl.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/sweetalert.min.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/bootstrap.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/tooltip.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/vista-previa.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/empresap.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/busqueda.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/perfilbk.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/comentariobk.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/sesionpv.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/uploadjs.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/bootstrap-datepicker.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/selecimg.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/msjclient.js"></script>
 <?php 
 	echo $_SERVER['REQUEST_URI'];
 	if ($_SERVER['REQUEST_URI'] != "/ptcprint/administracion") {
-		echo "<script type='text/javascript' src='vendor/mensajestimerback.js'></script>";
+		echo "<script type='text/javascript' src='/publico/js/vendor/mensajestimerback.js'></script>";
 	}
 ?>
--- publico/scripts.php
+++ publico/scripts.php
@@ -1,50 +1,50 @@
 <!--script para el modernizers-->
-<script type="text/javascript" src="vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 <!--SCRIPT PARA EL JQUERY-->
-<script type="text/javascript" src="vendor/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/jquery-1.11.2.min.js"></script>
 <!--<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>-->
 <!--ENALZO EL JQUERY DEL FRAMEWORK-->
-<script type="text/javascript" src="vendor/bootstrap.min.js"></script>
-<script type="text/javascript" src="vendor/uikit.min.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/bootstrap.min.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/uikit.min.js"></script>
 <!--<script type="text/javascript" src="publico/js/vendor/docs.js"></script>-->
 <!--ENLAZO MI SCRIPT-->
-<script type="text/javascript" src="js/main.js"></script>
+<script type="text/javascript" src="/publico/js/main.js"></script>
 <!--ENALZO LOS SCRIPTS PARA QUE LOS EFECTOS DE LA PAGINA FUNCIONEN-->
 <!--SON SCRIPTS QUE YA VENIAN CON EL FRAMEWORK-->
-<script type="text/javascript" src="components/upload.js"></script>
-<script type="text/javascript" src="components/slideshow.js"></script>
-<script type="text/javascript" src="components/slider.js"></script>
-<script type="text/javascript" src="components/slideshow-fx.js"></script>
-<script type="text/javascript" src="vendor/modal.js"></script>
-<script type="text/javascript" src="components/lightbox.js"></script>
-<script type="text/javascript" src="components/slideset.js"></script>
-<script type="text/javascript" src="components/tooltip.js"></script>
-<script type="text/javascript" src="components/form-password.js"></script>
-<script type="text/javascript" src="components/notify.js"></script>
-<script type="text/javascript" src="components/datepicker.js"></script>
-<script type="text/javascript" src="vendor/msjclient.js"></script>
-<script type="text/javascript" src="vendor/uploadcot.js"></script>
-<script type="text/javascript" src="vendor/carrito.js"></script>
-<script type="text/javascript" src="vendor/elegirp.js"></script>
+<script type="text/javascript" src="/publico/js/components/upload.js"></script>
+<script type="text/javascript" src="/publico/js/components/slideshow.js"></script>
+<script type="text/javascript" src="/publico/js/components/slider.js"></script>
+<script type="text/javascript" src="/publico/js/components/slideshow-fx.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/modal.js"></script>
+<script type="text/javascript" src="/publico/js/components/lightbox.js"></script>
+<script type="text/javascript" src="/publico/js/components/slideset.js"></script>
+<script type="text/javascript" src="/publico/js/components/tooltip.js"></script>
+<script type="text/javascript" src="/publico/js/components/form-password.js"></script>
+<script type="text/javascript" src="/publico/js/components/notify.js"></script>
+<script type="text/javascript" src="/publico/js/components/datepicker.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/msjclient.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/uploadcot.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/carrito.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/elegirp.js"></script>
 <!--ENLAZO EL SCRIPT DEL MENÚ-->
-<script type="text/javascript" src="vendor/menu-print.js"></script>
-<script type="text/javascript" src="vendor/acciones_front.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/menu-print.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/acciones_front.js"></script>
 <!--ENLAZO EL SCRIPT DEL MENÚ
 <script type="text/javascript" src="publico/js/vendor/canvas.js"></script>-->
-<script type="text/javascript" src="vendor/sweetalert.min.js"></script>
-<script type="text/javascript" src="vendor/perfilusr.js"></script>
-<script type="text/javascript" src="vendor/sesion.js"></script>
-<script type="text/javascript" src="vendor/comentarios.js"></script>
-<script type="text/javascript" src="js/siguiente.js"></script>
-<script type="text/javascript" src="js/menu.js"></script>
-<script type="text/javascript" src="vendor/errorreg.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/sweetalert.min.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/perfilusr.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/sesion.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/comentarios.js"></script>
+<script type="text/javascript" src="/publico/js/siguiente.js"></script>
+<script type="text/javascript" src="/publico/js/menu.js"></script>
+<script type="text/javascript" src="/publico/js/vendor/errorreg.js"></script>
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <?php 
 	echo $_SERVER['REQUEST_URI'];
 	if ($_SERVER['REQUEST_URI'] == "/ptcprint/iniciarsesion" || $_SERVER['REQUEST_URI'] == "/ptcprint/registrarme") {
 	}else{
 		if ($nombre != "Iniciar sesión") {
-			echo "<script type='text/javascript' src='vendor/mensajestimer.js'></script>";	
+			echo "<script type='text/javascript' src='/publico/js/vendor/mensajestimer.js'></script>";	
 		}
 	}
 ?>
