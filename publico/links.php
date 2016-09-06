f199dfbf7c595109912c719d7639a287ffe47490
OSLEZ <oslizama@gmail.com>
2016-09-05 19:33:17 -0600

    LINKS

--- publico/links.php
+++ publico/links.php
@@ -10,15 +10,15 @@
 <link rel="stylesheet" href="/publico/css/bootstrap.css">
 <link rel="stylesheet" href="/publico/css/sweetalert.css">
 <!--ESTILOS PERSONALIZADOS-->
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<link rel="stylesheet" type="text/css" href="css/main_G.css">
-<link rel="stylesheet" type="text/css" href="css/elegirp.css">
-<link rel="stylesheet" href="css/estilo.css">
-<link rel="stylesheet" href="css/menu/flaticon.css">
-<link rel="stylesheet" href="css/animate.css">
-<link rel="stylesheet" href="css/footer.css">
-<link rel="stylesheet" href="css/titulos.css">
-<link rel="stylesheet" href="css/botones.css">
+<link rel="stylesheet" type="text/css" href="/publico/css/main.css">
+<link rel="stylesheet" type="text/css" href="/publico/css/main_G.css">
+<link rel="stylesheet" type="text/css" href="/publico/css/elegirp.css">
+<link rel="stylesheet" href="/publico/css/estilo.css">
+<link rel="stylesheet" href="/publico/css/menu/flaticon.css">
+<link rel="stylesheet" href="/publico/css/animate.css">
+<link rel="stylesheet" href="/publico/css/footer.css">
+<link rel="stylesheet" href="/publico/css/titulos.css">
+<link rel="stylesheet" href="/publico/css/botones.css">
 <!--COMPATIBILIDAD CON IE Y DEMAS EXPLORADORES-->
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <!--ICONO DE LAP PAGINA-->
